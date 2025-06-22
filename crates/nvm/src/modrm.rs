8b797ba1ab65d613cce0f0bb1dee248c182aeca5
TStrecha <strechatomas@outlook.com>
2025-06-22 09:00:06 +0200

    code cleanup

--- crates/nvm/src/modrm.rs
+++ crates/nvm/src/modrm.rs
@@ -25,7 +25,7 @@ pub fn decode_operands_from_mod_rm_opcode(opcode_byte: u8, mem_slice: &[u8]) -> 
 
     let reg = Register::from_register_code(reg_bits, is_8_bit)?;
 
-    if mod_bits == 0b11000000 {
+    if is_reg_only(mod_bits) {
         let rm = Register::from_register_code(rm_bits, is_8_bit)?;
         return if is_rm_target {
             Ok((Operand::Register(rm), Operand::Register(reg)))
@@ -34,35 +34,7 @@ pub fn decode_operands_from_mod_rm_opcode(opcode_byte: u8, mem_slice: &[u8]) -> 
         }
     }
 
-    let mut displacement_size = mod_bits >> 6;
-    let (base_reg, index_reg) = match rm_bits {
-        0b000 => (Some(Register::BX), Some(Register::SI)),
-        0b001 => (Some(Register::BX), Some(Register::DI)),
-        0b010 => (Some(Register::BP), Some(Register::SI)),
-        0b011 => (Some(Register::BP), Some(Register::DI)),
-        0b100 => (None, Some(Register::SI)),
-        0b101 => (None, Some(Register::DI)),
-        x if x == 0b110 && mod_bits == 0 => {
-            displacement_size = 2;
-            (None, None)
-        }
-        0b111 => (Some(Register::BX), None),
-        _ => unreachable!(),
-    };
-
-    let displacement = match displacement_size {
-        0 => 0,
-        1 => mem_slice[1] as u16,
-        2 => u16::from_le_bytes([mem_slice[1], mem_slice[2]]),
-        _ => unreachable!(),
-    };
-
-    let rm_operand = Operand::Memory(MemAddress {
-        base: base_reg,
-        index: index_reg,
-        displacement,
-        displacement_size,
-    });
+    let rm_operand = extract_memory_address_operand(rm_bits, mod_bits, mem_slice);
 
     if is_rm_target {
         Ok((rm_operand, Operand::Register(reg)))
@@ -76,11 +48,15 @@ pub fn decode_operand_from_single_mod_rm_opcode(mem_slice: &[u8], is_8_bit: bool
     let mod_bits = modrm_byte & 0b11000000;
     let rm_bits = modrm_byte & 0b00000111;
 
-    if mod_bits == 0b11000000 {
+    if is_reg_only(mod_bits) {
         let rm = Register::from_register_code(rm_bits, is_8_bit)?;
         return Ok(Operand::Register(rm))
     }
 
+    Ok(extract_memory_address_operand(rm_bits, mod_bits, mem_slice))
+}
+
+pub fn extract_memory_address_operand(rm_bits: u8, mod_bits: u8, mem_slice: &[u8]) -> Operand {
     let mut displacement_size = mod_bits >> 6;
     let (base_reg, index_reg) = match rm_bits {
         0b000 => (Some(Register::BX), Some(Register::SI)),
@@ -104,12 +80,14 @@ pub fn decode_operand_from_single_mod_rm_opcode(mem_slice: &[u8], is_8_bit: bool
         _ => unreachable!(),
     };
 
-    let rm_operand = Operand::Memory(MemAddress {
+    Operand::Memory(MemAddress {
         base: base_reg,
         index: index_reg,
         displacement,
         displacement_size,
-    });
+    })
+}
 
-    Ok(rm_operand)
+pub fn is_reg_only(mod_bits: u8) -> bool {
+    mod_bits == 0b11000000
 }
